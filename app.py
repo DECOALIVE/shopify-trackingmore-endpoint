7b8c24d286b060b995cc2a1bb519e949eaf9a69e
DECOALIVE <tienda@decoalive.com>
2025-07-03 09:05:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +9,8 @@ def webhook():
     print("Evento recibido de TrackingMore:")
     print(data)
     return "OK", 200
+
+# Este bloque garantiza que Flask escuche correctamente en Render
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
